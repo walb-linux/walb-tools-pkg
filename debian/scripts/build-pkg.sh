7fc08f32d1c27c9b4d0fb015a9e3c4d97316168f
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2018-06-29 15:01:23 +0900

    add debian directory.

---
+++ debian/scripts/build-pkg.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DEBIAN_VER=$1
+
+if [ "$DEBIAN_VER" = "" ]; then
+    echo "Specify debian version like '1.0.0~beta1-0xenial1'."
+    exit 1
+fi
+
+gbp dch -N ${DEBIAN_VER} --ignore-branch --commit
+#gbp dch -N ${DEBIAN_VER} --ignore-branch --commit --release -D bionic
+gbp buildpackage --git-tag-only --git-ignore-branch
+sh debian/scripts/gbp-build.sh
