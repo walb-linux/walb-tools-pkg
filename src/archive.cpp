355d4c7268929a9c6effd4eb0b6841c7ef52f644
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2018-09-11 18:16:50 +0900

    archive: put more verbose logs for prepareRawFullScanner and prepareVirtualFullScanner.

--- src/archive.cpp
+++ src/archive.cpp
@@ -24,6 +24,11 @@ void prepareRawFullScanner(
             throw cybozu::Exception(__func__) << "bad sizeLb" << sizeLb << lv.sizeLb();
         }
     }
+    if (useCold) {
+        LOGs.info() << __func__ << lv.path() << gid;
+    } else {
+        LOGs.info() << __func__ << lv.path();
+    }
     file.open(lv.path().str());
 }
 
@@ -49,8 +54,12 @@ void prepareVirtualFullScanner(
         fileV, volInfo, allowEmpty, st0, [&](const MetaState &st) {
             return volInfo.getDiffMgr().getDiffListToSync(st, snap);
         });
-    LOGs.debug() << "virtual-full-scan-diffs" << st0 << diffV;
-
+    LOGs.debug() << __func__ << st0 << diffV;
+    if (diffV.empty()) {
+        LOGs.info() << __func__ << "empty";
+    } else {
+        LOGs.info() << __func__ << "numDiff" << diffV.size() << "firstDiff" << diffV[0];
+    }
     virt.init(std::move(fileR), std::move(fileV));
 }
 
